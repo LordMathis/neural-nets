47c3dd0637a87165566bc4e50f085c9952bdc46c
Mathis <matus@namesny.com>
2020-08-27 16:41:47 +0200

    Change logging

--- lib/utils.c
+++ lib/utils.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "utils.h"
 
-void log_info(const char *function_name, const char *message)
+void logger(int log_level, const char *function_name, const char *message)
 {
-    printf("%s: %s\n", function_name, message);
-}
-
-void log_exception(const char *function_name, const char *message)
-{
-    RED_COLOR;
-    printf("%s: %s\n", function_name, message);
-    RESET_COLOR;
+    if (log_level >= LOG_LEVEL)
+    {
+        if (log_level == EXCEPTION)
+        {
+            RED_COLOR;
+        }
+        printf("%s: %s\n", function_name, message);
+        RESET_COLOR;
+    }
 }
--- lib/utils.h
+++ lib/utils.h
@@ -1,11 +1,19 @@
 #ifndef UTILS_H
 
 #define UTILS_H
+
 #define RED_COLOR printf("\033[0;31m")
 #define GREEN_COLOR printf("\033[0;32m")
 #define RESET_COLOR printf("\033[0m" )
 
-void log_info(const char *function_name, const char *message);
-void log_exception(const char *function_name, const char *message);
+#define DEBUG 0
+#define INFO 1
+#define EXCEPTION 2
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL 1
+#endif
+
+void logger(int log_level, const char *function_name, const char *message);
 
 #endif
